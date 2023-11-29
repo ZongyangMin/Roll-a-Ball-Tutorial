eb5cc96e070640e1dca48248d23cd2bdb64fe2f2
Zongyang Min <mzongyang@gmail.com>
2023-11-28 23:45:04 -0500

    First Commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+ void Update()
+    {
+ // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+}
